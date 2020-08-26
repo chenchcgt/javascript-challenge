52f22a398095ad1cd36d9210409d866b85d5f92c
Hung Chen <chen_chc@hotmail.com>
2020-08-25 22:25:24 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # javascript-challenge
+
+## Purpose
+This Page is designed to retrieve data from UFO sights from dataset already included in folder. It will also have the capability of filter data by entering a date in the form.
+
+### Steps of utilizing this application:
+
+1 Copy the entire UFO-level-1 folder to your computer
+2 Open Visual Studio
+3 Navigate through index.html
+4 Launch the webpage by right clicking index.html and selecting "Open in default browser"
+5 A list of all UFO data will be shown
+6 To filter a specific date, enter date in form and hit "Enter" or click on button "Filter Table"
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,11 +5,12 @@ var button = d3.select("#filter-btn");
 
 var form = d3.select("form");
 
-// YOUR CODE HERE!
 // level 1 - auto table and date search
 var tbody = d3.select("tbody");
 console.log(data);
 
+
+// look through data per each ufolist. i.e. python for ufolist in data
 
 data.forEach(function(ufolist){
     console.log(ufolist);
@@ -22,34 +23,35 @@ data.forEach(function(ufolist){
     });
 });
 
-// create event handler
+
+// create event handler runEnter = name of function
 button.on("click",runEnter);
 // this is not working!!
 form.on("submit",runEnter);
 
 // function for event handler
 function runEnter(){
+// prevents the data to refresh, so "Enter" works from the Form
+    d3.event.preventDefault();
     var inputElement = d3.select("#datetime");
-    // get property - what is this for? where value comes from?!!
+    // get the input value
     var inputValue = inputElement.property("value");
 
     console.log(inputValue);
-    // console.log(tableData);
+
     var filterData = data.filter(data => data.datetime === inputValue);
 
     console.log(filterData);
 
-// clear page!!
-
-    // reenter values
-
+// clear page for filter data display
+    tbody.html("");
+// filter data function
     filterData.forEach(function(filterData){
         console.log(filterData);
         var row = tbody.append("tr");
     
         Object.entries(filterData).forEach(function([key,value]){
             console.log(key,value);
-            // cell.innerHTML = "";
             var cell = row.append("td");
             cell.text(value);
 
